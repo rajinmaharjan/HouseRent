8059ad9ccdc9a2412d9c21851ad072aa5697c258
rajinmaharjan <mjnrajin.18@gmail.com>
2022-06-11 14:48:23 +0545

    login page

---
+++ lib/pages/loginPage.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:house_rent_app/pages/signupPage.dart';
+import 'package:house_rent_app/reuseable_widgets/reuseable_widgets.dart';
+import 'package:house_rent_app/utils/colors_utils.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          hexStringColor("5ECC54"),
+          hexStringColor("954c64"),
+          hexStringColor("5ECCFF")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * .2, 20, 0),
+            child: Column(
+              children: <Widget>[
+                logoWidget("assets/images/houseLogo.png"),
+                SizedBox(
+                  height: 30,
+                ),
+                reuseableTextField("Enter email or username",
+                    Icons.person_outline, false, _emailTextController),
+                SizedBox(
+                  height: 20,
+                ),
+                reuseableTextField("Enter Password", Icons.lock_outline, true,
+                    _passwordTextController),
+                SizedBox(
+                  height: 20,
+                ),
+                loginSignUpButton(context, true, () {}),
+                signUpOption(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "Don't have an account ?",
+          style: TextStyle(color: Colors.white),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => SignUpPage()));
+          },
+          child: const Text(
+            "Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+}
